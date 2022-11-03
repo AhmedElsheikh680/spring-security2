faa337dd8ca404cf5f55d38d94db5f17a9cf080c
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-03 12:08:35 +0200

    config apis with spring security

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -9,9 +9,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests)->requests.anyRequest().authenticated());
-        http.formLogin();
-        http.httpBasic();
+//        http.authorizeRequests((requests)->requests.anyRequest().permitAll());
+//        http.formLogin();
+//        http.httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers("/about/*").permitAll()
+                .antMatchers("/connect/*").permitAll()
+                .antMatchers("/basketball/*").authenticated()
+                .antMatchers("/football/*").authenticated()
+                .antMatchers("/subscribers/*").authenticated()
+                .antMatchers("/swimming/*").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+
 
     }
 }
