3e7b4d3e06143c77344a8b770e65e064304243c6
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-03 12:36:02 +0200

    adding users inMemoryAuthentication

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.spring.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,18 +15,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        http.formLogin();
 //        http.httpBasic();
 
-        http.authorizeRequests((requests)->requests.anyRequest().denyAll()).formLogin().and().httpBasic();
+//        http.authorizeRequests((requests)->requests.anyRequest().denyAll()).formLogin().and().httpBasic();
 
-//        http.authorizeRequests()
-//                .antMatchers("/about/*").permitAll()
-//                .antMatchers("/connect/*").permitAll()
-//                .antMatchers("/basketball/*").authenticated()
-//                .antMatchers("/football/*").authenticated()
-//                .antMatchers("/subscribers/*").authenticated()
-//                .antMatchers("/swimming/*").authenticated()
-//                .and().formLogin()
-//                .and().httpBasic();
+        http.authorizeRequests()
+                .antMatchers("/about/*").permitAll()
+                .antMatchers("/connect/*").permitAll()
+                .antMatchers("/basketball/*").authenticated()
+                .antMatchers("/football/*").authenticated()
+                .antMatchers("/subscribers/*").authenticated()
+                .antMatchers("/swimming/*").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
 
 
+    }
+
+    //I can Add users In Memory from AuthenticationManagerBuilder
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("Ahmed").password("12345").authorities("admin")
+                .and().withUser("Mohamed").password("12345").authorities("player")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
