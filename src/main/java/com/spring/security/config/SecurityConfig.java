a1225505e4b47e5d3009ddaa275702f20ece9bbb
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-03 13:49:31 +0200

    spring security using db

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -7,9 +7,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -38,18 +42,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //I can Add users In Memory from AuthenticationManagerBuilder
 
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("Ahmed").password("12345").authorities("admin")
-//                .and().withUser("Mohamed").password("12345").authorities("player")
-//                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+////        auth.inMemoryAuthentication().withUser("Ahmed").password("12345").authorities("admin")
+////                .and().withUser("Mohamed").password("12345").authorities("player")
+////                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+//
+//        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+//       UserDetails userDetails =  User.withUsername("Ahmed").password("12345").authorities("admin").build();
+//      UserDetails userDetails2 =  User.withUsername("Mohamed").password("12345").authorities("player").build();
+//      inMemoryUserDetailsManager.createUser(userDetails);
+//      inMemoryUserDetailsManager.createUser(userDetails2);
+//      auth.userDetailsService(inMemoryUserDetailsManager);
+//    }
 
-        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
-       UserDetails userDetails =  User.withUsername("Ahmed").password("12345").authorities("admin").build();
-      UserDetails userDetails2 =  User.withUsername("Mohamed").password("12345").authorities("player").build();
-      inMemoryUserDetailsManager.createUser(userDetails);
-      inMemoryUserDetailsManager.createUser(userDetails2);
-      auth.userDetailsService(inMemoryUserDetailsManager);
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
