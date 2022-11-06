c7bce4fcbed9d9533cc15b12d8a818a4bb156b8f
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-06 10:13:46 +0200

    using new BcryptPasswordEncoder

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -60,8 +61,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    public UserDetailsService userDetailsService(DataSource dataSource){
 //        return new JdbcUserDetailsManager(dataSource);
 //    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
