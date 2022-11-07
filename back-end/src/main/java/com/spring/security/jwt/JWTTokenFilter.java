458a2e2c750c7644cb9f6fc316b91b006db01eae
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-07 16:40:15 +0200

    JWT Token Filter

---
+++ back-end/src/main/java/com/spring/security/jwt/JWTTokenFilter.java
@@ -0,0 +1,52 @@
+package com.spring.security.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JWTTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication !=null){
+           SecretKey secretKey =  Keys.hmacShaKeyFor(SecurityConstant.KEY.getBytes(StandardCharsets.UTF_8));
+           String jwt =  Jwts.builder().setSubject("Jwt Token")
+                    .claim("email", authentication.getName())
+                    .claim("authorities", authorities(authentication.getAuthorities()))
+                   .setIssuedAt(new Date())
+                   .setExpiration(new Date((new Date().getTime()+ 30000)))
+                   .signWith(secretKey).compact();
+
+           response.setHeader(SecurityConstant.HEADER, jwt);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return !request.getServletPath().equals("/login");
+    }
+
+    private String authorities(Collection<? extends GrantedAuthority> collections){
+        Set<String> auth = new HashSet<>(); //"user,admin"
+        for (GrantedAuthority authority: collections){
+            auth.add(authority.getAuthority());
+        }
+        return String.join(",", auth);
+    }
+}
---
+++ back-end/src/main/java/com/spring/security/jwt/SecurityConstant.java
@@ -0,0 +1,7 @@
+package com.spring.security.jwt;
+
+public class SecurityConstant {
+
+    public static final String KEY= "Ahmed";
+    public static final String HEADER = "Authorization";
+}
